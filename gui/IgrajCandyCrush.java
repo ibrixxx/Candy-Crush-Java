af67d6273dd2e47a6e0d5c2df3282c17deb03ce3
Ibrahim <53277964+ibrixxx@users.noreply.github.com>
2021-02-25 20:57:16 +0100

    Update IgrajCandyCrush.java

--- gui/IgrajCandyCrush.java
+++ gui/IgrajCandyCrush.java
@@ -7,12 +7,12 @@ public class IgrajCandyCrush {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		JFrame okvir = new JFrame("Candy Crush");
+		MojPanel mp = new MojPanel(5, 5, 10);
+		okvir.setContentPane(mp);
 		okvir.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		okvir.setLocationRelativeTo(null);
 		okvir.setVisible(true);
 		okvir.setSize(500, 600);
-		MojPanel mp = new MojPanel(5, 5, 10);
-		okvir.setContentPane(mp);
 	}
 
 }
