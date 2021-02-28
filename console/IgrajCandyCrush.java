2be4deef39349675390b4d4ddfd415c3256e6d9d
Ibrahim <53277964+ibrixxx@users.noreply.github.com>
2021-02-28 21:09:10 +0100

    Update IgrajCandyCrush.java

--- console/IgrajCandyCrush.java
+++ console/IgrajCandyCrush.java
@@ -4,13 +4,30 @@ import java.util.Scanner;
 
 import Logika.CandyCrush;
 
+/**
+ * 
+ * @author ibrahim
+ *
+ */
+
 public class IgrajCandyCrush {
 
+	/**
+	 * Main za konzolu.
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		
 		Scanner s = new Scanner(System.in);
+		int a, b, c;
+		System.out.println("Unesite broj redova: ");
+		a = s.nextInt();
+		System.out.println("Unesite broj kolona: ");
+		b = s.nextInt();
+		System.out.println("Unesite broj poteza: ");
+		c = s.nextInt();
 		
-		CandyCrush cc = new CandyCrush(6, 6, 10);
+		CandyCrush cc = new CandyCrush(a, b, c);
 		prikaziStanje(cc);
 		while(!cc.kraj()) {
 			int[] potez = new int[4];
@@ -25,9 +42,16 @@ public class IgrajCandyCrush {
 			prikaziStanje(cc);
 		}
 	}
-
+	
+	/**
+	 * Metoda za prikaz trenutnog stanja
+	 * tabele igrice u konzulu.
+	 * @param cc CandyCrush instanca logike igrice.
+	 */
 	private static void prikaziStanje(CandyCrush cc) {
 		int[][] mat = cc.tabelaStanja();
+		System.out.println("Broj bodova: " + cc.dajBodove());
+		System.out.println("Preostalo poteza: " + cc.dajPoteze());
 		for(int i = 0; i <mat.length; i++) {
 			for(int j = 0; j<mat[i].length; j++) {
 				System.out.print(mat[i][j]);
