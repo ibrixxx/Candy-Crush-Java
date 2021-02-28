5e23a215a779d37712fae759d8ebf14cfbeb6d37
Ibrahim <53277964+ibrixxx@users.noreply.github.com>
2021-02-28 21:07:45 +0100

    Update Dugmad.java

--- gui/Dugmad.java
+++ gui/Dugmad.java
@@ -9,50 +9,68 @@ import javax.swing.JPanel;
 
 import Logika.CandyCrush;
 
+/**
+ * 
+ * @author ibrahim
+ *
+ */
 public class Dugmad extends JPanel{
 	CandyCrush igra;
-	JButton restart;
-	JButton refresh;
+	JButton restartuj;
+	JButton refreshaj;
 	MojPanel polje;
 	InfoPanel info;
 	final static int maxBrojRefreshanja = 3;
 	int brojRefreshanja;
 	
-	public Dugmad(CandyCrush cc, MojPanel mp, InfoPanel ip) {
+	/**
+	 * Konstruktor sa četiri parametra koji postavlja
+	 * dugmad za kontrolu igrice.
+	 * @param cc CandyCrush instanca logike.
+	 * @param mp MojPanel panel za prikaz stanja tabele.
+	 * @param ip InfoPanle panel za prikaz informacija o igrici.
+	 * @param o Okvir prozor igrice.
+	 */
+	public Dugmad(CandyCrush cc, MojPanel mp, InfoPanel ip, Okvir o) {
 		brojRefreshanja = 0;
 		igra = cc;
 		polje = mp;
 		info = ip;
-		refresh = new JButton("refresh");
-		restart = new JButton("restart");
-		refresh.addActionListener(new ActionListener() {
+		refreshaj = new JButton("Osvježi  (" + maxBrojRefreshanja + ")");
+		restartuj = new JButton("Igraj ponovo");
+		refreshaj.addActionListener(new ActionListener() {
 			@Override
+			/**
+			 * Mijenja raspored bombona na polju bez dodavanja bodova i oduzimanja broja poteza.
+			 */
 			public void actionPerformed(ActionEvent arg0) {
 				if(igra.kraj())
 					brojRefreshanja = maxBrojRefreshanja;
 				if(brojRefreshanja < maxBrojRefreshanja) {
+					brojRefreshanja++;
+					int pom = igra.dajBodove();
 					igra.randomTabela();
+					igra.postaviBodove(pom);
 					polje.osvjeziStanje();
+					refreshaj.setText("Osvježi  (" + (maxBrojRefreshanja-brojRefreshanja) + ")");
 				}
-				brojRefreshanja++;
 			}
 		});
-		restart.addActionListener(new ActionListener() {
+		restartuj.addActionListener(new ActionListener() {
 			@Override
+			/**
+			 * Vraća igricu na početak.
+			 */
 			public void actionPerformed(ActionEvent arg0) {
-				int red = igra.tabelaStanja().length;
-				int kolona = igra.tabelaStanja()[0].length;
-				int potezi = igra.dajPotezeUkupno();
-				igra = new CandyCrush(red, kolona, potezi);
-				info = new InfoPanel(igra);
-				polje = new MojPanel(igra, info);
-				polje.osvjeziStanje();
 				brojRefreshanja = 0;
+				refreshaj.setText("Osvježi  (" + (maxBrojRefreshanja-brojRefreshanja) + ")");
+				igra.restart();
+				polje.osvjeziStanje();
 			}
 		});
 		setLayout(new GridLayout(1,2));
 		setVisible(true);
-		add(refresh);
-		add(restart);
+		add(refreshaj);
+		add(restartuj);
 	}
 }
